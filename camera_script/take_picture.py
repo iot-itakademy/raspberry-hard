9b58235d8c2241950a038d3522b28b8af3c7d248
matvki <m.vecchi@it-students.fr>
2022-06-24 15:14:54 +0200

    create an image with a datetime name and move it

--- camera_script/take_picture.py
+++ camera_script/take_picture.py
@@ -1,14 +1,24 @@
 import datetime
+import os
+import shutil
 import time
-import picamera
+import libcamera
 
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
+
 try:
     camera.start_preview()
     # Camera warm-up time
     time.sleep(2)
-    camera.capture('foo.jpg')
-    print(datetime.datetime)
+    date = datetime.datetime.now()
+    fileName = str(date.year) + '-' + str(date.month) + '-' + str(date.day) + '_T' + str(date.hour) + '-' + str(date.minute) + '-' + str(date.second) + '_MManual.jpg'
+    resize = (2592, 1944)
+    camera.capture(fileName, 'jpg', None, False, resize)
+    file_source = '/home/admin/Desktop/script/' + fileName
+    file_destination = '/home/admin/Images/security' + fileName
+
+    shutil.move(file_source, file_destination)
+
 finally:
     camera.close()
