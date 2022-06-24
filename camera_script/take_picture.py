5c8c9bc7929e25f432365988b2ee55e82a256ee3
matvki <m.vecchi@it-students.fr>
2022-06-24 15:52:14 +0200

    wip

--- camera_script/take_picture.py
+++ camera_script/take_picture.py
@@ -1,24 +1,31 @@
+# import all lib required
 import datetime
 import os
 import shutil
 import time
 import picamera
 
+# create camera object and defined his resolution
 camera = picamera.PiCamera()
 camera.resolution = (2592, 1944)
 
+# try finally to create a picture with a specified name and move it at a specified place
 try:
+    # open connection with the camera
     camera.start_preview()
-    # Camera warm-up time
     time.sleep(2)
+
+    # create the name of the picture
     date = datetime.datetime.now()
     fileName = str(date.year) + '-' + str(date.month) + '-' + str(date.day) + '_T' + str(date.hour) + '-' + str(date.minute) + '-' + str(date.second) + '_MManual.jpeg'
-    resize = (2592, 1944)
-    camera.capture(fileName, 'jpeg', None, False, resize)
+
+    # take the picture
+    camera.capture(fileName, 'jpeg')
+
+    # move the picture
     file_source = '/home/admin/Desktop/script/' + fileName
     file_destination = '/home/admin/Images/security/' + fileName
-
     shutil.move(file_source, file_destination)
-
 finally:
+    # close connection with the camera
     camera.close()
