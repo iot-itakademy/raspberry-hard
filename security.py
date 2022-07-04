ee690f38a5dc3347cba439bff92b0417964d90c5
matvki <m.vecchi@it-students.fr>
2022-07-04 18:27:47 +0200

    wip v0.3 - call api

--- security.py
+++ security.py
@@ -1,7 +1,11 @@
+import threading
 import RPi.GPIO as GPIO
 import time
+import json
 from take_picture import take_picture
-import urllib.request, json
+from urllib import request
+import requests
+
 
 # minimumDistance = requests.get('https://').json()
 
@@ -9,8 +13,15 @@ try:
     # setup motion sensore
     GPIO.setmode(GPIO.BCM)
 
+    try:
+        with request.urlopen("http://www.scrutoscope.live/api/settings") as url:
+            data = json.loads(url.read().decode())
+    except:
+        print('error')
+
     Trig = 23
     Echo = 24
+    amountPicture = data[0]['amountCapture']
 
     GPIO.setup(Trig, GPIO.OUT)
     GPIO.setup(Echo, GPIO.IN)
@@ -21,9 +32,11 @@ try:
     callApi = 0
     while True:
         if callApi == 0:
-            with urllib.request.urlopen("http://www.scrutoscope.live/api/settings/camera/1") as url:
-                data = json.loads(url.read().decode())
-
+            try:
+                with request.urlopen("http://www.scrutoscope.live/api/settings/camera/1") as url:
+                    data = json.loads(url.read().decode())
+            except:
+                print('error')
             minimumDistance = json.loads((data[0]['params']))['distance']
             width = json.loads((data[0]['params']))['width']
             height = json.loads((data[0]['params']))['height']
@@ -36,7 +49,6 @@ try:
         GPIO.output(Trig, True)
         time.sleep(0.00001)
         GPIO.output(Trig, False)
-
         while GPIO.input(Echo) == 0:  # send ultrasound
             startImpulse = time.time()
 
@@ -44,11 +56,17 @@ try:
             endImpulse = time.time()
 
         distance = int(round((endImpulse - startImpulse) * 340 * 100 / 2, 1))  # calculate distance (cm)
-        print('distance: ' + str(distance), 'previous: ' + str(previous))
 
         # if the new distance is less than the previous or if the new distance is less than 2 meters it takes a picture
         if (distance < previous and distance <= 10) or distance <= minimumDistance:
-            take_picture(width, height, pictureType)
+            threads = [threading.Thread(target=take_picture(width, height, pictureType)) for _ in range(amountPicture)]
+            [thread.start() for thread in threads]
+            [thread.join() for thread in threads]
+
+            url = 'http://www.scrutoscope.live/api/Statistics/post'
+            data = {"amount": amountPicture, "type": pictureType}
+
+            req = requests.post(url, json=data)
 
         previous = distance
 finally:
